478f07298cb005aeec85069365939c5d0d0fac72
codewiztinsing <tinsingjobs2k@gmail.com>
2024-04-13 17:48:16 +0300

    adding password validation

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/two-factor/', include('allauth_2fa.urls')),
     path('accounts/', include('allauth.urls')),
-    # path('accounts/signup/', AhfSignupView.as_view(), name='account_signup'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
