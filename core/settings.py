e526ae76a83c6fc1fa97158399f0df89bf36768f
codewiztinsing <tinsingjobs2k@gmail.com>
2024-04-18 09:04:37 +0300

    csrf token

--- core/settings.py
+++ core/settings.py
@@ -14,10 +14,12 @@ SECRET_KEY = 'django-insecure-xapy24exjy5!26b1d+7$v5wkh+44&(3rk^tfd=cbj#_$+dfb31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["54.151.110.104","127.0.0.1","18.144.126.117", "www.ahf.mortgage"]
+ALLOWED_HOSTS = [
+    'https://www.ahf.mortgage',
+    "www.ahf.mortgage"]
 CSRF_TRUSTED_ORIGINS = [
   
-    'http://www.ahf.mortgage',
+    # 'http://www.ahf.mortgage',
     'https://www.ahf.mortgage'
     ]
 
