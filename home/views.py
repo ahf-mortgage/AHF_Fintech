8d29816e0dfe2d74b9395ba3bfa87af9edf90841
Tinsae Alako <tinsae.alako@kegeberewtech.com>
2024-05-05 06:43:37 +0300

    updating

--- home/views.py
+++ home/views.py
@@ -227,6 +227,7 @@ def home(request):
     bps_to_gci_dict             = {}
     bps_to_ahf_commission_dict  = {}
     bps_to_branch_commission_dict = {}
+
     
     for key,value in zip(bps_from_50_to_250,gci_for_bps_from_50_to_250):
         bps_to_gci_dict[key] = value
@@ -236,6 +237,34 @@ def home(request):
         
     for key,value in zip(bps_from_50_to_250,branch_for_bps_from_50_to_250):
         bps_to_branch_commission_dict[key] = value
+        
+        
+        
+        
+    # lower  loan break points data
+    # range of numbers from 50 to 250 within 50 interval
+    #gci =IF(B10>0,$C$8/B10*10000,0 )
+    bps_from_50_to_250          = [50,100] + [num for num in range(100,250,25)]+[250]
+    gci_for_bps_from_50_to_250  = [gci/(num) * 10000 for num in bps_from_50_to_250] 
+    ahf_for_bps_from_50_to_250  = [(1 - branch.commission) * num for num in bps_from_50_to_250] 
+    branch_for_bps_from_50_to_250 = [branch.commission* num for num in bps_from_50_to_250]
+    bps_to_gci_dict             = {}
+    bps_to_ahf_commission_dict  = {}
+    bps_to_branch_commission_dict = {}
+
+    
+    for key,value in zip(bps_from_50_to_250,gci_for_bps_from_50_to_250):
+        bps_to_gci_dict[key] = value
+        
+    for key,value in zip(bps_from_50_to_250,ahf_for_bps_from_50_to_250):
+        bps_to_ahf_commission_dict[key] = value
+        
+    for key,value in zip(bps_from_50_to_250,branch_for_bps_from_50_to_250):
+        bps_to_branch_commission_dict[key] = value
+        
+        
+        
+        
         
         
   
