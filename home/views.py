8cfcac7ee96239ca257c8e8061cae48d1f019920
Tinsae Alako <tinsae.alako@kegeberewtech.com>
2024-05-07 22:51:57 +0300

    number which makes balance zero

--- home/views.py
+++ home/views.py
@@ -186,9 +186,9 @@ def home(request):
     }
   
    
-  
-    
-    q22.value = 92  #90.21346444444443
+    # q22 range(1-90) increment 1 
+    # q22 range(90 - 100) increment x
+    q22.value = 95
     balance = calculate_balance(branch_gross,total_expense,q22)
    
     increment = 1
@@ -261,69 +261,63 @@ def home(request):
 
     #         p = p + 1
     #     j = j + 1
-    p = 0
-    i = 0
-    k = 0 
-    j = 0    
-    while abs(balance) > 0.001 and j < 40:
+      
+
+
+
+        
+    while abs(balance) > 0.001 and j < 10:
         print("j= ",j)
-        print("++++++++++++++++++++ outer  while loop balance > 0")
+        print("========================================")
 
         print("q22a= ",q22.value)
         print("balance= ",balance)
         print("incrementa= ",increment)
-        # i = 0
-        print("ia= ",i)
+        i = 0
+        print("i= ",i)
 
         print("incrementb= ",increment)
         print("q22b= ",q22.value)
         print("balance ",balance)
 
+        print("+++++++++++++++++++++++++++++++++++++++++++")
         # balance is too low, need to raise to q22
         while balance > 0 and i < 160:
-            print("++++++++++++++++ first while loop balance > 0")
-
-            increment = increment * 10
-            print("increment= ",increment)
-            q22.value = q22.value - increment
-            print("q22c= ",q22.value)
-
+            q22.value = q22.value + increment
+            # increment = 
+            # print("increment= ",increment)
             balance = calculate_balance(branch_gross,total_expense,q22)
+
+            print("incrementc= ",increment)
+            print("q22c= ",q22.value)
             print("balance ",balance)
-            
+            k = 0
             # balance is still too low need to raise q22
-            
             while balance > 0 and k < 80:
-                print("++++++++++++++++++++++  second while loop balance > 0")
                 q22.value = q22.value + increment
                 print("q22K= ",q22.value)
                 balance = calculate_balance(branch_gross,total_expense,q22)
                 print("balanceK= ",balance)
+                
                 k = k + 1
-                print("k= ",k)
 
-       
-            
+            i = i + 1
         #balance is too high,need to lower q22
-       
-        # print("i= ",i)
+        i = 0
         while balance < 0 and i < 80:
-            print("++++++++++++++++++++++++++++++++++++++++++++ third while loop balance < 0")
             q22.value = q22.value - increment + increment/2
             increment = increment / 10
             print("incrementd= ",increment)
-            # if increment < S0.0001:
-                # print("increment < 0.0001")
-                # break
+            if increment < 0.0001:
+                print("increment < 0.0001")
+                break
             balance = calculate_balance(branch_gross,total_expense,q22)
             print("q22F ",q22.value)
             print("balance ",balance)
             
             # balance is still too high , need to lower q22 by current increment
-
-            # p = 0 
-            while balance < 0 and p < 10:
-                print("+++++++++++++++++++++++++++++++++++++++++++fourth while loop balance < 0")
+            p = 0 
+            while balance < 0 and p < 40:
                 q22.value = q22.value - increment
                 balance = calculate_balance(branch_gross,total_expense,q22)
 
@@ -332,13 +326,9 @@ def home(request):
                 print("balance ",balance)
 
                 p = p + 1
-                print("p= ",p)
             i = i + 1
-            print("ib= ",i)
 
         j = j + 1
-        print("j= ",j)
-        
       
 
         
