a94e6ab6ec9511d7995b6e348788bedff0b56c81
Tinsae Alako <tinsae.alako@kegeberewtech.com>
2024-05-13 21:26:08 +0300

    Compensation and max relation

--- recruiter/views.py
+++ recruiter/views.py
@@ -142,35 +142,31 @@ def comp_plan_change_view(request):
    
 
     if request.method == "POST":
-        max_gci = request.POST.get('max_gci',None)
         Maximum_Compensation = request.POST.get("Maximum_Compensation",None)
+        max_gci = request.POST.get('max_gci',None)
         comp_plan = request.POST.get("comp_plan")
         loan_break= request.POST.get("loan_break_point",0)
         branch_amount = request.POST.get("branch_amount")
-        CURRENT_Maximum_Compensation = comp_plan_obj.Maximum_Compensation
+        # CURRENT_Maximum_Compensation = comp_plan_obj.Maximum_Compensation
         
         if branch_amount != None:
             branch_amount = float(branch_amount)
             if branch_amount > 99:
                 branch_amount = 99
-                
-        print("*********DEBUG*************CURRENT_Maximum_Compensation = ",CURRENT_Maximum_Compensation)
-        print("*********DEBUG*************max_gci                      = ",max_gci)
-        print("*********DEBUG*************Maximum_Compensation         = ",Maximum_Compensation)
-                
+     
         # if max_gci != None and Maximum_Compensation != None:
-        #     if float(Maximum_Compensation) > CURRENT_Maximum_Compensation:
-        #         max_gci = CURRENT_Maximum_Compensation
+        if float(max_gci) > float(Maximum_Compensation):
+            max_gci = Maximum_Compensation
         
         if branch_amount:
             comp_plan_obj.MAX_GCI             = max_gci
+            comp_plan_obj.Maximum_Compensation    = float(Maximum_Compensation)
             comp_plan_obj.Percentage          = float(comp_plan)
             loan_break_point.loan_break_point = loan_break
             branch_amount                     = int(branch_amount) / 100
             branch.commission                 = branch_amount
             branch.save()
             loan_break_point.save()
-            comp_plan_obj.Maximum_Compensation = max_gci
             comp_plan_obj.save()
             return redirect("/")   
         else:
